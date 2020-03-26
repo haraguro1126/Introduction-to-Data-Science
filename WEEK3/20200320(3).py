7dcf397340911110f293238ea27898a209f1c638
haraguro1126 <61697645+haraguro1126@users.noreply.github.com>
2020-03-26 15:43:27 +0800

    WEEK3

---
+++ WEEK3/20200320(3).py
@@ -0,0 +1,7 @@
+#將電影的三個資訊儲存為物件並印出來,片長`評分`片名
+movie_time="181"
+movie_rating="8.5"
+movie_title="Avengers: Endgame"
+print(movie_time)
+print(movie_rating)
+print(movie_title)
---
+++ WEEK3/20200320(4).py
@@ -0,0 +1,4 @@
+#使用type()函數將這三個物件的類別印出來
+print(type(movie_time))
+print(type(movie_rating))
+print(type(movie_title))
