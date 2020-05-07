faf21eeceae85accab0e33feb3d81a2386728b28
haraguro1126 <61697645+haraguro1126@users.noreply.github.com>
2020-05-08 00:42:59 +0800

    WEEK9

---
+++ 20200501.py
@@ -0,0 +1,18 @@
+#隨1
+x = int(input("請輸入起始的整數:"))
+y = int(input("請輸入終止的整數:"))
+odd_summation = 0 # 歸零
+odd_counter = 0
+i = x # start
+while i <= y: # stop
+    # task
+    if i % 2 == 1:
+        odd_counter += 1 # 計數累計
+        odd_summation += i # 數值累加
+        print("現在的整數是:{}, 奇數計數為{}個, 總和為{}".format(i, odd_counter, odd_summation))
+    else:
+        print("現在的整數是:{}, 奇數計數為{}個, 總和為{}".format(i, odd_counter, odd_summation))
+    i += 1 # step
+print("======")
+print(odd_counter)
+print(odd_summation)
